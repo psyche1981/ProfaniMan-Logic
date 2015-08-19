649c740be94fc100c908d40f8b76e157b22406ac
psyche1981 <spjones1981@gmail.com>
2015-08-19 18:30:12 +0100

    welcome message changed back

--- src/psyche/games/profaniman/Main.java
+++ src/psyche/games/profaniman/Main.java
@@ -28,7 +28,7 @@ public class Main
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to ProfaniMan");
+		System.out.println("Welcome.....");
 		initialise();
 		initBoolArr();		
 		initAnswer();
